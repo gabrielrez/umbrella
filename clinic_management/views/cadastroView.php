72e53ce5dd2d811671346c2f5e5688c2c286e84c
gabrielrez <97976458+gabrielrez@users.noreply.github.com>
2024-05-22 09:44:32 -0300

    view cadastro

---
+++ clinic_management/views/cadastroView.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Umbrella - Cadastro</title>
+  <link rel="stylesheet" href="/clinic_management/public/styles/autentication/cadastro.css">
+</head>
+
+<body class="">
+  <header id="home" class="header-bg">
+    <div class="header">
+      <img class="header-logo" src="/clinic_management/public/midia//img/umbrella-logo.svg">
+    </div>
+  </header>
+
+  <section>
+    <h1 class="login-title poppins-semibold c11">Criar Conta</h1>
+    <form method="post">
+      <div class="input-container">
+        <label class="roboto-regular">Nome</label>
+        <input type="text" class="roboto-regular" placeholder="Nome*" required>
+      </div>
+      <div class="input-container">
+        <label class="roboto-regular">Email</label>
+        <input type="email" class="roboto-regular" placeholder="Email*" required>
+      </div>
+      <div class="input-container">
+        <label class="roboto-regular">Senha</label>
+        <input type="password" class="roboto-regular" placeholder="Senha*" required>
+      </div>
+      <button type="submit" class="sign-up-btn poppins-semibold c01">Entrar</button>
+    </form>
+  </section>
+</body>
+
+</html>
